8901bf409466daafa294db64c26f5b5521e7a56d
Andrey <nefedovandrey1997@gmail.com>
2022-05-22 19:36:29 +0300

    add button in mainTabBar

--- Twitter Clone MVVM/Controller/MainTabController.swift
+++ Twitter Clone MVVM/Controller/MainTabController.swift
@@ -11,11 +11,23 @@ class MainTabController: UITabBarController {
     
     // MARK: - Properties
     
+    private lazy var actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 56 / 2
+        button.clipsToBounds = true
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTabBar()
+        configureUI()
     }
 }
 
@@ -23,13 +35,26 @@ class MainTabController: UITabBarController {
 
 extension MainTabController {
     
+    private func configureUI() {
+        view.addSubview(actionButton)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            actionButton.heightAnchor.constraint(equalToConstant: 56),
+            actionButton.widthAnchor.constraint(equalToConstant: 56),
+            actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64),
+            actionButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16)
+        ])
+    }
+    
     private func configureTabBar() {
         configureViewControllers()
         configureTabBarColor()
     }
     
     private func configureViewControllers() {
-        
         let feed = FeedController()
         let feedNavigation = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
         
@@ -43,7 +68,6 @@ extension MainTabController {
         let conversationsNavigation = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootViewController: conversations)
         
         viewControllers = [feedNavigation, exploreNavigation, notificationNavigation, conversationsNavigation]
-        
     }
     
     private func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
@@ -62,16 +86,13 @@ extension MainTabController {
         nav.navigationBar.scrollEdgeAppearance = appearance
         
         return nav
-        
     }
     
     private func configureTabBarColor() {
-        
         UITabBar.appearance().backgroundColor = .systemGray6
         tabBar.backgroundImage = UIImage()
         
         tabBar.tintColor = .systemBlue
         tabBar.unselectedItemTintColor = .white
-        
     }
 }
---
+++ Twitter Clone MVVM/Utils/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Twitter Clone MVVM
+//
+//  Created by Andrey on 22.05.2022.
+//
+
+import UIKit
+
